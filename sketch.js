7e8854b109f53091c4f14042510465c1bb0a252d
Hchambers150 <Hchambers150@hotmail.co.uk>
2024-12-23 02:58:55 +0000

    umm idk what i'm doing tbh

--- sketch.js
+++ sketch.js
@@ -145,14 +145,13 @@ function putBack(){
 
 function shakeScreen(frame){ // original coordinates ?? of each sprite?
 
-    if(frame == 0){
+  if(frame == 0){
     // print(sprites);
     // getPositions();
     og = sprites;
     ogPos = getPositions();
     timerG.timerSprite.width -= punishAmount;
     score -= 50;
-
     try{
       errorsound1.play();
       errorsound2.play();
@@ -168,6 +167,7 @@ function shakeScreen(frame){ // original coordinates ?? of each sprite?
     correct = 0;
     putBack();
     background(51);
+    return(0);
 
     try{
       errorsound1.stop();
@@ -175,7 +175,6 @@ function shakeScreen(frame){ // original coordinates ?? of each sprite?
     } catch(error){
       console.log("nosounds!")
     }
-
     return(0);
   }
   // print(frame)
@@ -240,7 +239,6 @@ function keyPressed(){
 let winAmount = 15;
 
 function doCorrect(){
-
   try{
     winsound1.play();
   } catch(error2){"no sound!"}
@@ -258,12 +256,12 @@ function doCorrect(){
     if(active.length >= 1){
           trueActive = active[0];
     } else { // TIMERSPACE
-      try{  winsound2.play();}catch(error){"no sound!"}
+            try{  winsound2.play();}catch(error){"no sound!"}
       print("hoya!",(timerG.timerSprite.width += winAmount*5) < timerG.timerSprite.maxWidth)
       moreDiff();
       if((timerG.timerSprite.width += winAmount*5) < timerG.timerSprite.maxWidth){
         timerG.timerSprite.width += winAmount*5;
-
+        try{  winsound2.play();}catch(error){"no sound!"}
       } else {
         timerG.timerSprite.width = timerG.timerSprite.maxWidth;
       }
